972e3426b456ffbe3c40e30bbaebb7f4a7ebbf96
Josh Andromidas <66661368+andromidasj@users.noreply.github.com>
2023-04-09 22:59:31 -0500

    remove music from gitignore

---
+++ src/pages/music.tsx
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import { ParallaxBanner } from "react-scroll-parallax";
+import MusicSection from "~/components/MusicSection";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+export default function Portfolio() {
+  return (
+    <>
+      <Head>
+        <title>Parker Holt Portfolio | 801 Audio</title>
+        <meta
+          name="description"
+          content="Parker Holt's Digital Audio Portfolio | 801 Audio"
+        />
+        <link rel="preload" href="/assets/portfolio-banner.jpeg" as="image" />
+      </Head>
+
+      {/* Music */}
+      <ParallaxBanner
+        layers={[
+          { image: "/assets/hero-music.jpeg", speed: -20 },
+          {
+            speed: -3,
+            // shouldAlwaysCompleteAnimation: true,
+            children: (
+              <div className="absolute inset-0 flex items-center justify-center">
+                <h1 className="font-header text-6xl uppercase drop-shadow md:text-[100px]">
+                  Music
+                </h1>
+              </div>
+            ),
+          },
+        ]}
+        className="h-96 w-full"
+      />
+
+      <div className="m-auto max-w-7xl p-8">
+        <MusicSection />
+      </div>
+    </>
+  );
+}
