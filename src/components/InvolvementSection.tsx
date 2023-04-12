6c7f15bd30ea76fcc438ce20f7aad8ff906f0c85
Josh Andromidas <66661368+andromidasj@users.noreply.github.com>
2023-04-12 11:03:49 -0500

    remove unused code

--- src/components/InvolvementSection.tsx
+++
@@ -1,9 +0,0 @@
-export default function InvolvementSection() {
-  return (
-    <>
-      <h1 className="my-16 text-center font-header text-4xl uppercase text-white">
-        Involvement
-      </h1>
-    </>
-  );
-}
