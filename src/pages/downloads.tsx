d518c91045a4d255ca32d90f1dd0ddb52c82a1c9
Josh Andromidas <66661368+andromidasj@users.noreply.github.com>
2024-01-25 20:17:56 -0600

    nice message

--- src/pages/downloads.tsx
+++ src/pages/downloads.tsx
@@ -1,4 +1,8 @@
 export default function Downloads() {
   // hard code password Thunderband15!
-  return <div>prkr</div>;
+  return (
+    <div className="m-auto mt-16 flex max-w-xl flex-col gap-8 p-8">
+      Downloads coming soon!
+    </div>
+  );
 }
